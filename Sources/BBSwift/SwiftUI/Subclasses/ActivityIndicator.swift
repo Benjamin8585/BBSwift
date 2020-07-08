8943f4f025215ad968c8e05099c2e782e127e18e
Benjamin Bourasseau <82.benjamin@prendssoin.com>
2020-07-08 18:55:48 +0900

    Updating AI (activity indicator hihi)

--- Sources/BBSwift/SwiftUI/Subclasses/ActivityIndicator.swift
+++ Sources/BBSwift/SwiftUI/Subclasses/ActivityIndicator.swift
@@ -14,15 +14,21 @@ public struct ActivityIndicator: UIViewRepresentable {
 
     @Binding var isAnimating: Bool
     
+    public var color: Color?
     public let style: UIActivityIndicatorView.Style
     
-    public init(isAnimating: Binding<Bool>, style: UIActivityIndicatorView.Style) {
+    public init(isAnimating: Binding<Bool>, style: UIActivityIndicatorView.Style, color: Color? = nil) {
         self._isAnimating = isAnimating
         self.style = style
+        self.color = color
     }
 
     public func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
-        return UIActivityIndicatorView(style: style)
+        let view = UIActivityIndicatorView(style: style)
+        if let color = color {
+            view.color = color.uiColor()
+        }
+        return view
     }
 
     public func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
