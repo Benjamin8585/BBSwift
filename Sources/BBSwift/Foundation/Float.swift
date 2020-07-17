b8833539fc3701f26f3e2b4a9c924290180fb4e3
Benjamin Bourasseau <82.benjamin@prendssoin.com>
2020-07-17 19:45:29 +0900

    Adding randomness

---
+++ Sources/BBSwift/Foundation/Float.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by Benjamin Bourasseau on 2020/07/17.
+//
+
+import Foundation
+
+public extension Float {
+    
+    /// Returns a random floating point number between 0.0 and 1.0, inclusive.
+    static var random: Float {
+        return Float(arc4random()) / 0xFFFFFFFF
+    }
+
+    /// Random float between 0 and n-1.
+    ///
+    /// - Parameter n:  Interval max
+    /// - Returns:      Returns a random float point number between 0 and n max
+    static func random(min: Float, max: Float) -> Float {
+        return Float.random * (max - min) + min
+    }
+    
+}
--- Sources/BBSwift/Foundation/Int.swift
+++ Sources/BBSwift/Foundation/Int.swift
@@ -8,6 +8,24 @@
 import Foundation
 
 public extension Int {
+
+    /// Random integer between 0 and n-1.
+    ///
+    /// - Parameter n:  Interval max
+    /// - Returns:      Returns a random Int point number between 0 and n max
+    static func random(n: Int) -> Int {
+        return Int(arc4random_uniform(UInt32(n)))
+    }
+
+    ///  Random integer between min and max
+    ///
+    /// - Parameters:
+    ///   - min:    Interval minimun
+    ///   - max:    Interval max
+    /// - Returns:  Returns a random Int point number between 0 and n max
+    static func random(min: Int, max: Int) -> Int {
+        return Int.random(n: max - min + 1) + min
+    }
     
     /// Trasnform value to hour min seconds
     func secondsToHoursMinutesSeconds() -> (hour: Int, min: Int, sec: Int) {
