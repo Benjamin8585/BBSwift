9b9288d7d75acf561674f0584f697dd9b1d1c51d
Benjamin Bourasseau <82.benjamin@prendssoin.com>
2020-07-28 14:23:11 +0900

    Fixing some bugs

--- Sources/BBSwift/SwiftUI/Subclasses/NumberTextfield.swift
+++ Sources/BBSwift/SwiftUI/Subclasses/NumberTextfield.swift
@@ -21,24 +21,26 @@ public struct NumberTextField: UIViewRepresentable {
     var accentColor: Color?
     var type: NumberTextFieldType
     
-    var proxy: String {
-        get {
-            if let value = self.value {
-                if self.type == .integer {
-                    return String(format: "%.0f", Double(value))
+    var proxy: Binding<String> {
+         Binding<String>(
+             get: {
+                if let value = self.value {
+                    if self.type == .integer {
+                        return String(format: "%.0f", Double(value))
+                    } else {
+                        return String(format: "%.02f", Double(value))
+                    }
                 } else {
-                    return String(format: "%.02f", Double(value))
+                    return ""
                 }
-            } else {
-                return ""
-            }
-        }
-        set(val) {
-            if let value = NumberFormatter().number(from: val) {
-                self.value = value.doubleValue
-            }
-        }
-    }
+             },
+             set: {
+                 if let value = NumberFormatter().number(from: $0) {
+                     self.value = value.doubleValue
+                 }
+             }
+         )
+     }
     
     public init(placeholder: String, value: Binding<Double?>, type: NumberTextFieldType, accentColor: Color? = nil) {
         self.placeholder = placeholder
@@ -53,14 +55,14 @@ public struct NumberTextField: UIViewRepresentable {
         textfield.tintColor = self.accentColor?.uiColor()
         let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: textfield.frame.size.width, height: 44))
         let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(textfield.doneButtonTapped(button:)))
-        toolBar.items = [doneButton]
-        toolBar.setItems([doneButton], animated: true)
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolBar.setItems([space, doneButton], animated: true)
         textfield.inputAccessoryView = toolBar
         return textfield
     }
 
     public func updateUIView(_ uiView: UITextField, context: Context) {
-        uiView.text = self.proxy
+        uiView.text = self.proxy.wrappedValue
 
     }
 }
