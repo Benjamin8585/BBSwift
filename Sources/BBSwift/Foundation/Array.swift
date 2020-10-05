563e63ebc37c493fd7d9075ba58c9a68716b8e90
Benjamin Bourasseau <82.benjamin@prendssoin.com>
2020-10-05 12:07:57 +0900

    Adding zip3 function

--- Sources/BBSwift/Foundation/Array.swift
+++ Sources/BBSwift/Foundation/Array.swift
@@ -36,3 +36,65 @@ public extension Array {
         }
     }
 }
+
+struct Zip3Generator<A: IteratorProtocol, B: IteratorProtocol, C: IteratorProtocol>: IteratorProtocol {
+
+    private var first: A
+    private var second: B
+    private var third: C
+
+    private var index = 0
+
+    init(_ first: A, _ second: B, _ third: C) {
+        self.first = first
+        self.second = second
+        self.third = third
+    }
+
+    // swiftlint:disable large_tuple
+    mutating func next() -> (A.Element, B.Element, C.Element)? {
+        if let first = first.next(), let second = second.next(), let third = third.next() {
+            return (first, second, third)
+        }
+        return nil
+    }
+}
+
+func zip<A: Sequence, B: Sequence, C: Sequence>(_ first: A, _ second: B, _ third: C) -> IteratorSequence<Zip3Generator<A.Iterator, B.Iterator, C.Iterator>> {
+    return IteratorSequence(Zip3Generator(first.makeIterator(), second.makeIterator(), third.makeIterator()))
+}
+
+public struct ThreeDimension<T>: Identifiable {
+
+    public var id: UUID
+    public var elem1: T
+    public var elem2: T
+    public var elem3: T
+
+    public init(elem1: T, elem2: T, elem3: T) {
+        self.id = UUID()
+        self.elem1 = elem1
+        self.elem2 = elem2
+        self.elem3 = elem3
+    }
+}
+
+public extension Array {
+    func threeDimentions() -> [ThreeDimension<Element>] {
+        var one: [Element] = []
+        var two: [Element] = []
+        var three: [Element] = []
+        self.enumerated().forEach { (i, p) in
+            if i % 2 == 0 {
+                one.append(p)
+            } else if i % 2 == 1 {
+                two.append(p)
+            } else {
+                three.append(p)
+            }
+        }
+        return zip(one, two, three).map { (e1, e2, e3) -> ThreeDimension<Element> in
+            return ThreeDimension<Element>(elem1: e1, elem2: e2, elem3: e3)
+        }
+    }
+}
