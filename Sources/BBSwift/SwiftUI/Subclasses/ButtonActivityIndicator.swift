8a9324d6e0d31688e186148f10a3dffc3907be3a
Benjamin Bourasseau <82.benjamin@prendssoin.com>
2020-10-28 15:23:35 +0900

    Testing new way to solve loader issue

--- Sources/BBSwift/SwiftUI/Subclasses/ButtonActivityIndicator.swift
+++ Sources/BBSwift/SwiftUI/Subclasses/ButtonActivityIndicator.swift
@@ -18,22 +18,26 @@ public struct ButtonActivityIndicator: View {
 
     public var body: some View {
       GeometryReader { (geometry: GeometryProxy) in
-        ZStack(alignment: .center) {
-            ForEach(0..<5) { index in
-              Group {
-                Circle()
-                  .frame(width: geometry.size.width / 5, height: geometry.size.height / 5)
-                  .scaleEffect(!self.isAnimating ? 1 - CGFloat(index) / 5 : 0.2 + CGFloat(index) / 5)
-                  .offset(y: geometry.size.width / 10 - geometry.size.height / 2)
+        VStack(spacing: 0) {
+                ZStack(alignment: .center) {
+                    ForEach(0..<5) { index in
+                      Group {
+                        Circle()
+                          .frame(width: geometry.size.width / 5, height: geometry.size.height / 5)
+                          .scaleEffect(!self.isAnimating ? 1 - CGFloat(index) / 5 : 0.2 + CGFloat(index) / 5)
+                          .offset(y: geometry.size.width / 10 - geometry.size.height / 2)
+                        }.frame(width: geometry.size.width, height: geometry.size.height)
+                          .rotationEffect(!self.isAnimating ? .degrees(0) : .degrees(360))
+                          .animation(
+                            Animation.timingCurve(0.5, 0.15 + Double(index) / 5, 0.25, 1, duration: 1.5).repeatForever(autoreverses: false)
+                          )
+                    }
                 }.frame(width: geometry.size.width, height: geometry.size.height)
-                  .rotationEffect(!self.isAnimating ? .degrees(0) : .degrees(360))
-                  .animation(
-                    Animation.timingCurve(0.5, 0.15 + Double(index) / 5, 0.25, 1, duration: 1.5).repeatForever(autoreverses: false)
-                  )
-              }
-            }.frame(width: geometry.size.width, height: geometry.size.height)
-        }.aspectRatio(1, contentMode: .fit)
-        .onAppear {
+        }
+        .frame(width: geometry.size.width,
+               height: geometry.size.height,
+               alignment: .center)
+        }.onAppear {
             self.isAnimating = true
         }
     }
