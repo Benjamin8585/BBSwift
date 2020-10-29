e23318203083c724f6293b34915ef3e7698ad4f6
Benjamin Bourasseau <82.benjamin@prendssoin.com>
2020-10-29 13:09:42 +0900

    no message

--- Package.swift
+++ Package.swift
@@ -11,10 +11,10 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "BBSwift",
-            targets: ["BBSwift", "BBSwiftScroll"]),
-        .library(
-            name: "BBSwiftWatch",
             targets: ["BBSwift"]),
+        .library(
+            name: "BBSwiftFull",
+            targets: ["BBSwift", "BBSwiftScroll"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
