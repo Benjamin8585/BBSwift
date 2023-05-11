8ba86e0d664da89a61afe3cdebe6cff6103211b1
Benjamin Bourasseau <bourasseaubenjamin@gmail.com>
2023-05-11 16:04:01 +0200

    Adding hex initializer for Color

--- Sources/BBSwift/SwiftUI/Extensions/Color.swift
+++ Sources/BBSwift/SwiftUI/Extensions/Color.swift
@@ -39,5 +39,19 @@ public extension Color {
         }
         return (r, g, b, a)
     }
+    
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.scanLocation = 0
+
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+
+        let r = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+        let g = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+        let b = CGFloat(rgbValue & 0x0000FF) / 255.0
+
+        self.init(red: Double(r), green: Double(g), blue: Double(b))
+    }
 }
 #endif
